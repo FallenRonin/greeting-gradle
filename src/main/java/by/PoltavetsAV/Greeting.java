4784bc11e6822a40adb6eb140658681ed81c3ea9
FallenRonin <FallenRonin@yandex.ru>
2025-10-03 15:49:48 +0300

    Initial commit: Create Gradle project

---
+++ src/main/java/by/PoltavetsAV/Greeting.java
@@ -0,0 +1,9 @@
+package by.PoltavetsAV;
+
+public class Greeting {
+
+    public String greeting()
+    {
+        return "Hello MP-10";
+    }
+}
---
+++ src/test/java/by/PoltavetsAV/GreetingTest.java
@@ -0,0 +1,16 @@
+package by.PoltavetsAV;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GreetingTest {
+
+    @Test
+    void shouldGreeting(){
+        Greeting greeting = new Greeting();
+        String result = greeting.greeting();
+        assertEquals("Hello MP-10", result);
+    }
+
+}
